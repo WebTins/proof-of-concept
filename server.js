ba4da29fd53956ada9975f40885e68d11fc56e3f
Tin Nguyen <powertinnguyen@gmail.com>
2026-06-04 15:09:32 +0200

    #2 Server setup
    Server opgezet voor de routes

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express, { response } from 'express'
+import { Liquid } from 'liquidjs';
+
+// Express
+const app = express()
+app.use(express.static('public'))
+
+// Liquid
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+// Views
+app.set('views', './views')
+app.set('view engine', 'liquid')
+app.use(express.urlencoded({extended: true}))
+
+// BASE API link
+const pokeApi = 'https://pokeapi.co/api/v2/'
+// MARK: port 8000
+app.set('port', process.env.PORT || 8000)
+
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
