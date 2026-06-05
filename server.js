39bdd0db0bd754b0502029d4f1025a14b426df74
Tin Nguyen <powertinnguyen@gmail.com>
2026-06-05 09:26:41 +0200

    #2 Route aangemaakt HOME
    Home route aangemaakt en data opgehaald.

--- server.js
+++ server.js
@@ -16,6 +16,35 @@ app.use(express.urlencoded({extended: true}))
 
 // BASE API link
 const pokeApi = 'https://pokeapi.co/api/v2/'
+
+// MARK: Home page
+app.get ('/', async function (request, response) {
+
+    const pokeResponse = await fetch (`${pokeApi}/pokemon?limit=9`)
+    const pokeResponseJSON = await pokeResponse.json()
+
+    const pokemonDetails = await Promise.all(
+        pokeResponseJSON.results.map(async (pokemon) => {
+          const detailResponse = await fetch(pokemon.url)
+          const detailJSON = await detailResponse.json()
+
+          console.log(detailJSON);
+        
+          return {
+              name: detailJSON.name,
+              id: detailJSON.id,
+              image: detailJSON.sprites.other['official-artwork'].front_default,
+              types: detailJSON.types.map(t => t.type.name),
+        }
+      })
+    )
+// maak index.liquid om naar html en geef hier onderstaande data aan mee
+    response.render('index', {
+// de data die in de template mag, in me liquid kan ik dan {{} pokemon }} gebruiken
+    pokemon: pokemonDetails
+    })
+})
+
 // MARK: port 8000
 app.set('port', process.env.PORT || 8000)
 
